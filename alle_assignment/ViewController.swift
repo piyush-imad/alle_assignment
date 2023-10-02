e3d4e3b26632e482958b1a3de82158f66ff94ea5
piyush-imad <piyush9401@gmail.com>
2023-10-02 23:06:33 +0530

    Initial Commit

---
+++ alle_assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  alle_assignment
+//
+//  Created by Piyush Sharma on 02/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
